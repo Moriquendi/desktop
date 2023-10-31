ad842de87d1ce75b459ad9ddcfa7bcf7cff0a0d9
Michal <michal.smialko@gmail.com>
2023-10-31 09:27:12 -0700

    Updater wip windows

--- updater/mac/Updater.js
+++ updater/mac/Updater.js
@@ -23,15 +23,15 @@ class Updater {
 
   run() {
     const osVersion = require('os').release();
-    if (osVersion && Number(osVersion.substring(0, 2)) < 19) {
-      dialog.showMessageBoxSync({
-        message:
-          'You are on a very old version of macOS. Please update macOS to continue using Buffed Desktop. Your current version is outdated and is no longer compatible with Buffed services.',
-        type: 'error',
-      });
-      app.exit();
-      return;
-    }
+    // if (osVersion && Number(osVersion.substring(0, 2)) < 19) {
+    //   dialog.showMessageBoxSync({
+    //     message:
+    //       'You are on a very old version of macOS. Please update macOS to continue using Buffed Desktop. Your current version is outdated and is no longer compatible with Buffed services.',
+    //     type: 'error',
+    //   });
+    //   app.exit();
+    //   return;
+    // }
 
     this.updateState = {};
 
